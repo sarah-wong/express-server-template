d44c4e280ab24ca4668d6d387835a4b58dc80253
PerScholarSarah <rmandolin22@gmail.com>
2024-09-04 09:06:17 -0400

    removed passwords

--- models/user.js
+++ models/user.js
@@ -1,22 +1,16 @@
 const uniqueID = require('generate-unique-id');
 const validator = require('email-validator');
 
-const CASE_REGEX = /^(?=*[A-Z])(?=*[a-z])$/;
-const NUM_REGEX = /^(?=*[0-9])$/;
-const SPECHAR_REGEX = /^(?=*[^A-Za-z0-9])$/;
-const MIN_PASSWORD_LENGTH = 8;
-
 class User{
-    constructor(email, username, password){
+    constructor(email, username){
         this.email = email;
         this.username = username;
-        this.password = password; // TODO: Don't store passwords as plaintext!
     }
 }
 
 const users = {};
 
-function create(email, username, password){
+function create(email, username){
     console.log('Attempting to create new User...');
 
     // check email is valid
@@ -40,24 +34,8 @@ function create(email, username, password){
         console.log(`${username} is valid and unique, proceeding...`);
     }
 
-    // check password strength
-    if(password.length < MIN_PASSWORD_LENGTH){
-        throw new EvalError(`Password MUST be at least ${MIN_PASSWORD_LENGTH} characters long!`);
-    }
-    else if(!password.match(CASE_REGEX)){
-        throw new EvalError('Password MUST contain at least one uppercase and one lowercase letter!')
-    }
-    else if(!password.match(NUM_REGEX)){
-        throw new EvalError('Passowrd MUST contain at least one number!');
-    }
-    else if(!password.match(SPECHAR_REGEX)){
-        throw new EvalError(`Password MUST contain at least one special character`);
-    }
-    else{
-        console.log('strong password, proceeding...');
-    }
-
-    // create and cache new user
+    const id = uniqueID();
+    users.push(new User(email, username));
 }
 
 function read(id){
